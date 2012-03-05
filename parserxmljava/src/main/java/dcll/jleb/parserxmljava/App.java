3683f76581c9a755e213843cc61e7ac52869ed5f
Julien <Julien@130.120.242.250>
2012-03-05 15:53:59 +0100

    FirstCommit

---
+++ parserxmljava/src/main/java/dcll/jleb/parserxmljava/App.java
@@ -0,0 +1,13 @@
+package dcll.jleb.parserxmljava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
