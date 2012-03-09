f8b98274bd45946703db8fba77e5688cbb67f74f
William Ferraguti <homer.dizamok@laposte.net>
2012-03-09 23:32:33 +0100

    #19 Parser pour RegularAnswer

--- parserxmljava/src/main/java/dcll/answer/RegularAnswer.java
+++ parserxmljava/src/main/java/dcll/answer/RegularAnswer.java
@@ -20,7 +20,20 @@ public class RegularAnswer extends Answer {
 		this.feedback = feedback;
 	}
 	
-	
+	public Element parse() {
+		Element a = new Element("answer");
+		a.setAttribute("fraction", String.valueOf(fraction));
+		
+		Element e_text = new Element("text").setText(text);
+		a.addContent(e_text);
+		
+		Element e_feedb = new Element("feedback");
+		Element e_feed_text = new Element("text").setText(feedback);
+		e_feedb.addContent(e_feed_text);
+		a.addContent(e_feedb);
+		
+		return a;
+	}
 	
 
 }
