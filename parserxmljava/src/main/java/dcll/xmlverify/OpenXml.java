32e1f93b42263363581bce714bdfe3b668969bf6
Rigue <PierreRig@gmail.com>
2012-03-12 09:16:19 +0100

    #9 ajout d'exemple xml + test JDOM

---
+++ parserxmljava/src/main/java/dcll/xmlverify/OpenXml.java
@@ -0,0 +1,11 @@
+package dcll.xmlverify;
+
+public class OpenXml {
+
+	void testadeuxballesJDOM(){
+		// ok c'est bon librairie JDOM opérationnelle gràce au POM
+		// Méthode testeadeuxballes supprimées prochainement
+		org.jdom.Verifier.checkXMLName("jose.xml");
+		
+	}
+}
