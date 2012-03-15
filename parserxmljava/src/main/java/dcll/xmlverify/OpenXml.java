6d24769a79dd6327f9e2d4650329187d456fca23
Rigue <PierreRig@gmail.com>
2012-03-15 15:33:22 +0100

    #4 fonction open() dans openXML

--- parserxmljava/src/main/java/dcll/xmlverify/OpenXml.java
+++ parserxmljava/src/main/java/dcll/xmlverify/OpenXml.java
@@ -1,11 +1,45 @@
 package dcll.xmlverify;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
+
 public class OpenXml {
 
-	void testadeuxballesJDOM(){
-		// ok c'est bon librairie JDOM opérationnelle gràce au POM
-		// Méthode testeadeuxballes supprimées prochainement
-		org.jdom.Verifier.checkXMLName("jose.xml");
-		
+	private File xmlFile;
+	private SAXBuilder sxb;
+	private org.jdom.Document document=null;
+	
+	public OpenXml(String xmlFile){
+		this.xmlFile=new File(xmlFile);
+		this.sxb = new SAXBuilder(); 
 	}
+	
+	public void open(){		
+		try{
+			this.document=sxb.build(this.xmlFile);
+			// If there are no well-formedness or validity errors
+			// then no exception is thrown.
+			// return true = xml is valid
+		}
+		// indicates or validity error, but xml is wellformed
+		catch (JDOMException e){
+			System.out.println( this.xmlFile + " is not valid" );
+			System.out.println (e.getMessage());
+		}
+		catch (IOException e){
+			System.out.println("Could not check " + this.xmlFile);
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	/*public static void main(String[] args ){
+		
+		OpenXml op=new OpenXml("Documents\\quizExamples\\quizExample_1.xml");
+		System.out.println(op.isXmlWellFormed());
+		System.out.println(op.document.toString());
+	} */
+	
 }
