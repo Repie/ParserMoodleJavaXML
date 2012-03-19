97aded6b3297b840c44bbb5195949d82d0aa0322
Rigue <PierreRig@gmail.com>
2012-03-19 23:55:48 +0100

    #26 creation de la methode recuperant les balises intrus et creant le fichier xml

--- parserxmljava/src/main/java/dcll/xmlverify/OpenXml.java
+++ parserxmljava/src/main/java/dcll/xmlverify/OpenXml.java
@@ -1,17 +1,24 @@
 package dcll.xmlverify;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+
+import org.jdom.Document;
+import org.jdom.Element;
 import org.jdom.JDOMException;
 import org.jdom.input.SAXBuilder;
 
 
-public class OpenXml {
+public class OpenXml implements dcll.interfaces.ConstantsTAG{
 
 	private File xmlFile;
 	private SAXBuilder sxb;
-	private org.jdom.Document document=null;
+	private Document document=null;
 	
 	public OpenXml(String xmlFile){
 		this.xmlFile=new File(xmlFile);
@@ -25,7 +32,7 @@ public class OpenXml {
 			// then no exception is thrown.
 			// return true = xml is valid
 		}
-		// indicates or validity error, but xml is wellformed
+		// indicates a validity error or a no well-formedness error
 		catch (JDOMException e){
 			System.out.println( this.xmlFile + " is not valid" );
 			System.out.println (e.getMessage());
@@ -36,18 +43,80 @@ public class OpenXml {
 		}
 	}
 	
-	public org.jdom.Document checkedQuiz(){
-		org.jdom.Document verifiedQuiz=null;
+	public Document checkedQuiz() throws XMLException, XMLRootElementException, XMLNoQuestionsException{
+		Document verifiedQuiz=null;
 		
+		if(this.document==null) throw new XMLException();
+		else {
+			
+			Element root = this.document.getRootElement();
+			if(root.getName().equals(ROOT_ELEMENT)) throw new XMLRootElementException();
+			else{
+				verifiedQuiz = (Document) this.document.clone();
+				List <Element> questions = root.getChildren(); 
+				List <Element> intruders = null;
+				int nbQuestions = 0;
+				for(Element question : questions){
+					if(question.getName().equals(QUESTION_ELEMENT)){
+						nbQuestions ++;
+					}
+					else{
+						verifiedQuiz.removeContent(question);
+						intruders.add(question);
+					}
+					
+				}
+				
+				if(nbQuestions==0) throw new XMLNoQuestionsException();
+				
+				if(!intruders.isEmpty()){
+					createFileIntruders(this.xmlFile.getName(),intruders);
+									
+				}
+			} 
+			
+			
+		}
 		
 		
 		return verifiedQuiz;
 	}
+	
+	
+	private void createFileIntruders(String nameFile, List<Element> intruders) {
+		// TODO Auto-generated method stub
+		String pathFile = System.getProperty("user.dir") + "\\IntrudersXML\\Intruders_"+nameFile;
+		try{
+			FileWriter fw = new FileWriter(pathFile, true);
+			BufferedWriter output = new BufferedWriter(fw);
+			
+			output.write("All the intruders of file : " + nameFile + "\n");
+			
+			for(Element intruder : intruders){
+				output.write(intruder.toString() +"\n");
+				output.write(intruder.getValue() +"\n");
+			}
+			
+			output.flush();
+			output.close();
+			
+		}
+		catch(IOException ioe){
+			System.out.print("Erreur : ");
+			ioe.printStackTrace();
+		}	
+	}
+
 /*	public static void main(String[] args ){
-		;
+		
 		OpenXml op=new OpenXml("Documents\\quizExamples\\quizExample_1.xml");
 		op.open();
-		System.out.println(op.document.toString());
-	} 
-	*/
+		Element test = new Element("quiz");
+		String pathFile = System.getProperty("user.dir") + "\\IntrudersXML\\Intruders_"+op.xmlFile.getName();
+		System.out.println(pathFile);
+		
+		op.createFileIntruders(op.xmlFile.getName(),op.document.getRootElement().getChildren());
+		
+	} */
+	
 }
